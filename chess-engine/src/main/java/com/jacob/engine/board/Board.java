7db083fc3d4c07828065eb6a44dce88bf991665d
Vedanth Mohan <vedanth2805@gmail.com>
2022-03-04 16:37:08 +0530

    Initial implementations for the chess board

---
+++ chess-engine/src/main/java/com/jacob/engine/board/Board.java
@@ -0,0 +1,67 @@
+package board;
+
+import pieces.King;
+import pieces.Knight;
+import pieces.Rook;
+import pieces.Bishop;
+import pieces.Queen;
+import pieces.Pawn;
+
+public class Board {
+    Spot[][] boxes;
+
+    public Board() {
+        boxes = new Spot[8][8];
+        this.resetBoard();
+    }
+
+    public Spot getBox(int x, int y) {
+        if(x < 0 || x > 7 || y < 0 || y > 7) {
+            throw new ArithmeticException("Index out of bound");
+        }
+
+        return boxes[x][y];
+    }
+
+    public void resetBoard() {
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 8; j++) {
+                boxes[i][j] = new Spot(i, j, null);
+            }
+        }
+        // initialize white pieces
+         boxes[0][0] = new Spot(0, 0, new Rook(true));
+        boxes[0][1] = new Spot(0, 1, new Knight(true));
+         boxes[0][2] = new Spot(0, 2, new Bishop(true));
+         boxes[0][3] = new Spot(0, 3, new Queen(true));
+        boxes[0][4] = new Spot(0, 4, new King(true));
+         boxes[0][5] = new Spot(0, 5, new Bishop(true));
+        boxes[0][6] = new Spot(0, 6, new Knight(true));
+         boxes[0][7] = new Spot(0, 7, new Rook(true));
+        // initialize white pawns
+        for(int j = 0; j < 8; j++) {
+             boxes[1][j] = new Spot(1, j, new Pawn(true));
+        }
+
+        // initialize black pieces
+        boxes[7][0] = new Spot(7, 0, new Rook(false));
+        boxes[7][1] = new Spot(7, 1, new Knight(false));
+        boxes[7][2] = new Spot(7, 2, new Bishop(false));
+        boxes[7][3] = new Spot(7, 3, new Queen(false));
+        boxes[7][4] = new Spot(7, 4, new King(false));
+        boxes[7][5] = new Spot(7, 5, new Bishop(false));
+        boxes[7][6] = new Spot(7, 6, new Knight(false));
+        boxes[7][7] = new Spot(7, 7, new Rook(false));
+        // initialize black pawns
+        for(int j = 0; j < 8; j++) {
+            boxes[6][j] = new Spot(6, j, new Pawn(false));
+        }
+
+        // initialize remaining boxes without any piece
+        for(int i = 2; i < 6; i++) {
+            for(int j = 0; j < 8; j++) {
+                boxes[i][j] = new Spot(i, j, null);
+            }
+        }
+    }
+}
