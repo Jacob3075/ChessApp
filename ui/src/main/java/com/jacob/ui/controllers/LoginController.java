d2e045f94eea9828dc4c119e382994b6d129daff
Jacob3075 <jacob.bosco3075@gmail.com>
2022-03-15 12:37:27 +0530

    Fixed issues in login page ui and controllers.

--- ui/src/main/java/com/jacob/ui/controllers/LoginController.java
+++ ui/src/main/java/com/jacob/ui/controllers/LoginController.java
@@ -1,97 +1,65 @@
-package com.example.login;
+package com.jacob.ui.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.control.Button;
-import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ResourceBundle;
 import java.net.URL;
+import java.util.ResourceBundle;
 
+@Component
 public class LoginController implements Initializable {
-    @FXML
-    private Button cancelButton;
-    @FXML
-    private Label loginMessageLabel;
-    @FXML
-    private ImageView chessImageView;
-    @FXML
-    private TextField usernameTextField;
-    @FXML
-    private PasswordField enterPasswordField;
+    @FXML private Button cancelButton;
+    @FXML private Button loginButton;
+    @FXML private Label loginMessageLabel;
+    @FXML private ImageView chessImageView;
+    @FXML private TextField usernameTextField;
+    @FXML private PasswordField enterPasswordField;
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle){
+    public void initialize(URL url, ResourceBundle resourceBundle) {
         File chessFile = new File("src/images/chessLogo.jpg");
         Image chessImage = new Image(chessFile.toURI().toString());
-        chessImageView.setImage(chessImage);
+        //        chessImageView.setImage(chessImage);
     }
 
-    public void loginButtonOnAction(ActionEvent event){
-        if (usernameTextField.getText().isBlank() == false && enterPasswordField.getText().isBlank() == false) {
-            validateLogin();
-        }
-        else{
+    public void loginButtonOnAction(ActionEvent event) {
+        if (!usernameTextField.getText().isBlank() && !enterPasswordField.getText().isBlank()) {
+        } else {
             loginMessageLabel.setText("Please enter username and password.");
         }
     }
 
-    public void cancelButtonOnAction(ActionEvent event){
+    public void cancelButtonOnAction(ActionEvent event) {
         Stage stage = (Stage) cancelButton.getScene().getWindow();
         stage.close();
     }
 
-    public void validateLogin(){
-        DatabaseConnection connectNow = new DatabaseConnection();
-        Connection connectDB = connectNow.getConnection();
+    /*
+       public void createAccountForm(){
+           try{
 
-        String verifyLogin = "SELECT count(1) FROM user_accounts WHERE username = '" + usernameTextField.getText() + "' AND password = '" + enterPasswordField.getText() + "'";
-        try{
-            Statement statement = connectDB.createStatement();
-            ResultSet queryResult = statement.executeQuery(verifyLogin);
+               FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("register_ui.fxml"));
+               Scene scene = new Scene(fxmlLoader.load(), 520, 400);
+               Stage registerStage = new Stage();
+               registerStage.initStyle(StageStyle.UNDECORATED);
+               registerStage.setScene(scene);
+               registerStage.show();
+           } catch(Exception e){
+               e.printStackTrace();
+               e.getCause();
+           }
 
-            while(queryResult.next()){
-                if (queryResult.getInt(1) == 1){
-                    loginMessageLabel.setText("Congratulations!");
-                    //createAccountForm();
-                }
-                else {
-                    loginMessageLabel.setText("Invalid login. Please try again.");
-                }
-            }
-
-        } catch (Exception e){
-            e.printStackTrace();
-            e.getCause();
-        }
-    }
-
-/*
-    public void createAccountForm(){
-        try{
-
-            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("register_ui.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(), 520, 400);
-            Stage registerStage = new Stage();
-            registerStage.initStyle(StageStyle.UNDECORATED);
-            registerStage.setScene(scene);
-            registerStage.show();
-        } catch(Exception e){
-            e.printStackTrace();
-            e.getCause();
-        }
-
-    }
- */
+       }
+    */
 
 }
--- ui/src/main/java/com/jacob/ui/events/StageInitializer.java
+++ ui/src/main/java/com/jacob/ui/events/StageInitializer.java
@@ -18,7 +18,7 @@ public class StageInitializer implements ApplicationListener<StageReadyEvent> {
     private final ApplicationContext context;
 
     public StageInitializer(
-            @Value("classpath:/main-screen.fxml") Resource fxml, ApplicationContext context) {
+            @Value("classpath:/login_ui.fxml") Resource fxml, ApplicationContext context) {
         this.fxml = fxml;
         this.context = context;
     }
@@ -30,7 +30,7 @@ public class StageInitializer implements ApplicationListener<StageReadyEvent> {
             FXMLLoader fxmlLoader = new FXMLLoader(fxml.getURL());
             fxmlLoader.setControllerFactory(context::getBean);
             Parent root = fxmlLoader.load();
-            Scene scene = new Scene(root, 600, 600);
+            Scene scene = new Scene(root, 520, 400);
             stage.setScene(scene);
             stage.show();
         } catch (IOException e) {
