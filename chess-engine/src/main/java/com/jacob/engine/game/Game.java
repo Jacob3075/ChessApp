a2bedc91247a559c61034d8af8354ec18831e37b
vedanth <vedanth2805@gmail.com>
2022-03-18 17:38:31 +0530

    added comments, removed extra lines from getEvaluation() and added check for whether the game is over

--- chess-engine/src/main/java/com/jacob/engine/game/Game.java
+++ chess-engine/src/main/java/com/jacob/engine/game/Game.java
@@ -11,36 +11,40 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Game {
-    private Player[] players;
-    private Board board;
+    private final Player[] players;
+    private final Board board;
     private Player currentTurn;
     private GameStatus status;
-    private List<Move> movesPlayed;
-    private List<Piece> piecesCapturedByPlayerZero;
-    private List<Piece> piecesCapturedByPlayerOne;
+    private final List<Move> movesPlayed;
+    private final List<Piece> piecesCapturedByPlayerZero;
+    private final List<Piece> piecesCapturedByPlayerOne;
 
     public Game(Player p1, Player p2) {
+        // initializing the players
         this.players = new Player[2];
         players[0] = p1;
         players[1] = p2;
 
+        // creating the board and other required fields
         this.board = new Board();
-
         this.movesPlayed = new ArrayList<>();
         this.piecesCapturedByPlayerZero = new ArrayList<>();
         this.piecesCapturedByPlayerOne = new ArrayList<>();
 
+        // assigning the current turn
         if(p1.isWhiteSide()) {
             this.currentTurn = p1;
         }
         else {
             this.currentTurn = p2;
         }
-
+        
+        // setting the game status as ACTIVE
         this.status = GameStatus.ACTIVE;
 
+        // main game loop
         Scanner in = new Scanner(System.in);
-        while(this.getStatus() == GameStatus.ACTIVE) {
+        while(!this.isEnd()) {
             if(currentTurn == players[0]) {
                 System.out.println("Player Zero's Move: ");
             }
@@ -53,12 +57,34 @@ public class Game {
                 System.out.println("Illegal move. Enter a different move: ");
                 isLegal = playerMove(currentTurn, in.nextInt(), in.nextInt(), in.nextInt(), in.nextInt());
             }
+            
             board.displayBoard();
             System.out.println(this.getEvaluation());
+
+            // checking if game is over
+            // generating all possible moves for the current player
+            List<Move> possibleMoves = currentTurn.generateMoves(board);
+
+            // if the current player cannot make a move
+            if(possibleMoves.isEmpty()) {
+                if(currentTurn == players[0]) {
+                    System.out.println("Player 1 wins!");
+                }
+                else {
+                    System.out.println("Player 0 Wins!");
+                }
+
+                if(currentTurn.isWhiteSide()) {
+                    this.setStatus(GameStatus.BLACK_WIN);
+                }
+                else {
+                    this.setStatus(GameStatus.WHITE_WIN);
+                }
+            }
         }
     }
 
-    public boolean isEnd() {
+    private boolean isEnd() {
         return this.getStatus() != GameStatus.ACTIVE;
     }
 
@@ -69,29 +95,32 @@ public class Game {
     public void setStatus(GameStatus status) {
         this.status = status;
     }
-    
-    public int getEvaluation() {
-        int evaluation = 0;
-//        int playerZeroPieceMultiplier = players[0].isWhiteSide() ? 1 : -1;
-//        int playerOnePieceMultiplier = players[1].isWhiteSide() ? 1 : -1;
-//
-//        for(Piece piece : piecesCapturedByPlayerZero) {
-//            evaluation += piece.getValue()*playerZeroPieceMultiplier;
-//        }
-//        for(Piece piece : piecesCapturedByPlayerOne) {
-//            evaluation += piece.getValue()*playerOnePieceMultiplier;
-//        }
 
+    private List<Piece> getPiecesCapturedByPlayerZero() {
+        return this.piecesCapturedByPlayerZero;
+    }
+
+    private List<Piece> getPiecesCapturedByPlayerOne() {
+        return this.piecesCapturedByPlayerOne;
+    }
+    
+    private int getEvaluation() {
+        int evaluation = 0;
+        
+        // iterating through all the spots and updating the rating based on the pieces on the board
         for(int i = 0; i < 8; i++) {
             for(int j = 0; j < 8; j++) {
-                Piece p = board.getSpot(i, j).getPiece();
-                if(p != null) {
-                    if(p.isWhite()) {
-                        evaluation += p.getValue();
-                    }
-                    else {
-                        evaluation -= p.getValue();
-                    }
+                // getting the piece on the current spot
+                Piece currentPiece = board.getSpot(i, j).getPiece();
+
+                // if the piece is white, increase the evaluation by the piece's value
+                if(currentPiece != null && currentPiece.isWhite()) {
+                    evaluation += currentPiece.getValue();
+                }
+
+                // if the piece is black, decrease the evaluation by the piece's value
+                else if(currentPiece != null && !currentPiece.isWhite()) {
+                    evaluation -= currentPiece.getValue();
                 }
             }
         }
@@ -99,7 +128,7 @@ public class Game {
         return evaluation;
     }
 
-    public boolean playerMove(Player player, int startJ, int startI, int endJ, int endI) {
+    private boolean playerMove(Player player, int startJ, int startI, int endJ, int endI) {
         Spot startSpot = board.getSpot(startI, startJ);
         Spot endSpot = board.getSpot(endI, endJ);
         Move move = new Move(player, startSpot, endSpot);
@@ -179,36 +208,18 @@ public class Game {
             // pawn promotion
             if(((Pawn) movedPiece).isPromotionPossible()) {
                 ((Pawn) movedPiece).setPromotionPossible(false);
+
                 // finding what piece to promote the pawn to
                 Scanner in = new Scanner(System.in);
                 System.out.println("Enter your choice:\n1. Queen\n2. Rook\n3. Bishop\n4. Knight");
                 int choice = in.nextInt();
-//                in.close();
 
                 // promoting the pawn
                 switch (choice) {
-                    case 1 -> {
-                        movedPiece = new Queen(movedPiece.isWhite());
-                    }
-                    case 2 -> {
-                        movedPiece = new Rook(movedPiece.isWhite());
-                    }
-                    case 3 -> {
-                        movedPiece = new Bishop(movedPiece.isWhite());
-                    }
-                    case 4 -> {
-                        movedPiece = new Knight(movedPiece.isWhite());
-                    }
-                }
-            }
-        }
-        else {
-            if(capturedPiece instanceof King) {
-                if(player.isWhiteSide()) {
-                    this.setStatus(GameStatus.WHITE_WIN);
-                }
-                else {
-                    this.setStatus(GameStatus.BLACK_WIN);
+                    case 1 -> movedPiece = new Queen(movedPiece.isWhite());
+                    case 2 -> movedPiece = new Rook(movedPiece.isWhite());
+                    case 3 -> movedPiece = new Bishop(movedPiece.isWhite());
+                    case 4 -> movedPiece = new Knight(movedPiece.isWhite());
                 }
             }
         }
@@ -245,8 +256,6 @@ public class Game {
         else {
             this.currentTurn = players[0];
         }
-
-        // TODO: check for checkmate
 
         return true;
     }
