d475415571db2140f33388f84821362a60703a93
vedanth <vedanth2805@gmail.com>
2022-03-29 18:36:15 +0530

    cleaned up the canMove method

--- chess-engine/src/main/java/com/jacob/engine/pieces/Knight.java
+++ chess-engine/src/main/java/com/jacob/engine/pieces/Knight.java
@@ -10,23 +10,21 @@ public class Knight extends Piece {
 
     @Override
     public boolean canMove(Board board, Spot start, Spot end) {
-        if(start == null || end == null || start == end) {
+        if(start == null || end == null || start == end)
             return false;
-        }
         
         Piece capturedPiece = end.getPiece();
 
         // we can't move the piece to a spot that has a piece of the same colour
-        if(capturedPiece != null && capturedPiece.isWhite() == this.isWhite()) {
+        if(capturedPiece != null && capturedPiece.isWhite() == this.isWhite())
             return false;
-        }
 
-        int di = Math.abs(start.getI() - end.getI());
-        int dj = Math.abs(start.getJ() - end.getJ());
+        int deltaI = Math.abs(start.getI() - end.getI());
+        int deltaJ = Math.abs(start.getJ() - end.getJ());
 
-        if(di * dj != 2) {
+        // for every possible knight move the product of deltaI and deltaJ is equal to 2
+        if(deltaI * deltaJ != 2)
             return false;
-        }
 
         // move is legal if player's king is not attacked after making it
         return !this.isKingAttackedAfterMove(board, start, end);
--- chess-engine/src/main/java/com/jacob/engine/pieces/Queen.java
+++ chess-engine/src/main/java/com/jacob/engine/pieces/Queen.java
@@ -10,16 +10,14 @@ public class Queen extends Piece {
 
     @Override
     public boolean canMove(Board board, Spot start, Spot end) {
-        if(start == null || end == null || start == end) {
+        if(start == null || end == null || start == end)
             return false;
-        }
         
         Piece capturedPiece = end.getPiece();
 
         // we can't move the piece to a spot that has a piece of the same colour
-        if(capturedPiece != null && capturedPiece.isWhite() == this.isWhite()) {
+        if(capturedPiece != null && capturedPiece.isWhite() == this.isWhite())
             return false;
-        }
 
         // since a queen moves either like a bishop or a rook, check to see if either is possible
         Bishop queenAsBishop = new Bishop(this.isWhite());
@@ -27,9 +25,8 @@ public class Queen extends Piece {
         Rook queenAsRook = new Rook(this.isWhite());
         boolean canQueenMoveAsRook = queenAsRook.canMove(board, start, end);
 
-        if(!(canQueenMoveAsBishop || canQueenMoveAsRook)) {
+        if(!(canQueenMoveAsBishop || canQueenMoveAsRook))
             return false;
-        }
 
         // move is legal if player's king is not attacked after making it
         return !this.isKingAttackedAfterMove(board, start, end);
