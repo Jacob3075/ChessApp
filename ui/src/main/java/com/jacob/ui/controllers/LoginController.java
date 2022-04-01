56181e312911fef1d0e322f5f5e64d22b01ae318
Jacob3075 <jacob.bosco3075@gmail.com>
2022-04-01 17:16:30 +0530

    Added navigating to register page in LoginController.

--- ui/src/main/java/com/jacob/ui/controllers/LoginController.java
+++ ui/src/main/java/com/jacob/ui/controllers/LoginController.java
@@ -1,34 +1,48 @@
 package com.jacob.ui.controllers;
 
-import com.jacob.ui.ChessAppUI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 @Component
 public class LoginController implements Initializable {
-    @FXML private Button cancelButton;
+    @FXML private Button registerButton;
     @FXML private Button loginButton;
     @FXML private Label loginMessageLabel;
     @FXML private TextField usernameTextField;
     @FXML private PasswordField enterPasswordField;
 
+    private final Resource registerSceneFxml;
+    private final ApplicationContext context;
+
+    public LoginController(
+            @Value("classpath:/register_ui.fxml") Resource registerSceneFxml,
+            ApplicationContext context) {
+        this.registerSceneFxml = registerSceneFxml;
+        this.context = context;
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         loginButton.setOnAction(this::loginButtonOnAction);
-        cancelButton.setOnAction(this::cancelButtonOnAction);
+        registerButton.setOnAction(this::showRegisterScreen);
     }
 
     private void loginButtonOnAction(ActionEvent event) {
@@ -42,21 +56,15 @@ public class LoginController implements Initializable {
         loginMessageLabel.setText("Logged in");
     }
 
-    private void cancelButtonOnAction(ActionEvent event) {
-        Stage stage = (Stage) cancelButton.getScene().getWindow();
-        stage.close();
-    }
-
-    public void createAccountForm() {
+    private void showRegisterScreen(@NotNull ActionEvent event) {
         try {
-            FXMLLoader fxmlLoader =
-                    new FXMLLoader(ChessAppUI.class.getResource("register_ui.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(), 520, 400);
-            Stage registerStage = new Stage();
-            registerStage.initStyle(StageStyle.UNDECORATED);
-            registerStage.setScene(scene);
-            registerStage.show();
-        } catch (Exception e) {
+            FXMLLoader registerFxmlLoader = new FXMLLoader(registerSceneFxml.getURL());
+            registerFxmlLoader.setControllerFactory(context::getBean);
+            Scene registerScene = new Scene(registerFxmlLoader.load());
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            window.setScene(registerScene);
+            window.show();
+        } catch (IOException e) {
             e.printStackTrace();
             e.getCause();
         }
