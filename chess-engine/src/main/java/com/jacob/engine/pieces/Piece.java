f94601fde830cb775ac353c375f2e3329788c526
vedanth <vedanth2805@gmail.com>
2022-04-02 16:07:29 +0530

    added separate methods to check if the kingSpot is attacked by a pawn or knight

--- chess-engine/src/main/java/com/jacob/engine/pieces/Piece.java
+++ chess-engine/src/main/java/com/jacob/engine/pieces/Piece.java
@@ -27,7 +27,7 @@ public abstract class Piece {
     }
 
     public void setSymbol(String symbol) {
-        if(isWhite())
+        if (isWhite())
             this.symbol = symbol.toUpperCase();
         else
             this.symbol = symbol.toLowerCase();
@@ -43,82 +43,38 @@ public abstract class Piece {
         return value;
     }
 
+    // moves the piece from the start spot to the end spot, and then checks if king is attacked
     public boolean isKingAttackedAfterMove(Board board, Spot start, Spot end) {
-        // moving the piece from the start spot to the end spot, then checking if king is attacked
         Piece pieceMoved = start.getPiece();
         Piece pieceCaptured = end.getPiece();
+        Piece attackingPiece;
         start.setPiece(null);
         end.setPiece(pieceMoved);
-
-        // a piece which could be attacking the king
-        Piece attackingPiece;
 
         // the coordinates of the attacking piece on the board
         int ti;
         int tj;
 
-        // the spot where the king is
-        Spot kingSpot = board.getSpot(0,0);
-        for(int i = 0; i < 8; i++) {
-            for(int j = 0; j < 8; j++) {
-                Piece piece = board.getSpot(i, j).getPiece();
-                if(piece instanceof King && piece.isWhite() == this.isWhite()) {
-                    kingSpot = board.getSpot(i, j);
-                }
-            }
-        }
+        Spot kingSpot = getKingSpot(board, start, end);
 
-        // if we are moving the king, we need to check if the end spot is being attacked
-        if(start.getPiece() instanceof King) {
-            kingSpot = end;
-        }
-
-        // attacked by pawn?
-        ti = this.isWhite() ? kingSpot.getI()+1 : kingSpot.getI()-1;
-        tj = kingSpot.getJ();
-        attackingPiece = ti<8 && ti>=0 && tj-1>=0 ? board.getSpot(ti, tj-1).getPiece() : null;
-        if(attackingPiece instanceof Pawn && attackingPiece.isWhite() != this.isWhite()) {
+        if(isKingSpotAttackedByPawn(board, kingSpot)
+            || isKingSpotAttackedByKnight(board, kingSpot)) {
             start.setPiece(pieceMoved);
             end.setPiece(pieceCaptured);
             return true;
-        }
-        attackingPiece = ti<8 && ti>=0 && tj+1<8 ? board.getSpot(ti, tj+1).getPiece() : null;
-        if(attackingPiece instanceof Pawn && attackingPiece.isWhite() != this.isWhite()) {
-            start.setPiece(pieceMoved);
-            end.setPiece(pieceCaptured);
-            return true;
-        }
-
-        // attacked by knight?
-        for(ti = kingSpot.getI()-2; ti <= kingSpot.getI()+2; ti++) {
-            for(tj = kingSpot.getJ()-2; tj <= kingSpot.getJ()+2; tj++) {
-                if(ti >= 0 && ti < 8 && tj >= 0 && tj < 8) {
-                    attackingPiece = board.getSpot(ti, tj).getPiece();
-                    int di = Math.abs(ti-kingSpot.getI());
-                    int dj = Math.abs(tj-kingSpot.getJ());
-
-                    if(di * dj == 2
-                        && attackingPiece instanceof Knight
-                        && attackingPiece.isWhite() != this.isWhite()) {
-                        start.setPiece(pieceMoved);
-                        end.setPiece(pieceCaptured);
-                        return true;
-                    }
-                }
-            }
         }
 
         // straight up
         attackingPiece = null;
         ti = kingSpot.getI()+1;
-        while(ti < 8) {
+        while (ti < 8) {
             attackingPiece = board.getSpot(ti, kingSpot.getJ()).getPiece();
-            if(attackingPiece != null) {
+            if (attackingPiece != null) {
                 break;
             }
             ti++;
         }
-        if(attackingPiece != null
+        if (attackingPiece != null
             && attackingPiece.isWhite() != this.isWhite()
             && (attackingPiece instanceof Rook
                 || attackingPiece instanceof Queen
@@ -131,14 +87,14 @@ public abstract class Piece {
         // straight down
         attackingPiece = null;
         ti = kingSpot.getI()-1;
-        while(ti >= 0) {
+        while (ti >= 0) {
             attackingPiece = board.getSpot(ti, kingSpot.getJ()).getPiece();
-            if(attackingPiece != null) {
+            if (attackingPiece != null) {
                 break;
             }
             ti--;
         }
-        if(attackingPiece != null
+        if (attackingPiece != null
             && attackingPiece.isWhite() != this.isWhite()
             && (attackingPiece instanceof Rook
                 || attackingPiece instanceof Queen
@@ -151,14 +107,14 @@ public abstract class Piece {
         // straight right
         attackingPiece = null;
         tj = kingSpot.getJ()+1;
-        while(tj < 8) {
+        while (tj < 8) {
             attackingPiece = board.getSpot(kingSpot.getI(), tj).getPiece();
-            if(attackingPiece != null) {
+            if (attackingPiece != null) {
                 break;
             }
             tj++;
         }
-        if(attackingPiece != null
+        if (attackingPiece != null
             && attackingPiece.isWhite() != this.isWhite()
             && (attackingPiece instanceof Rook
                 || attackingPiece instanceof Queen
@@ -171,14 +127,14 @@ public abstract class Piece {
         // straight left
         attackingPiece = null;
         tj = kingSpot.getJ()-1;
-        while(tj >= 0) {
+        while (tj >= 0) {
             attackingPiece = board.getSpot(kingSpot.getI(), tj).getPiece();
-            if(attackingPiece != null) {
+            if (attackingPiece != null) {
                 break;
             }
             tj--;
         }
-        if(attackingPiece != null
+        if (attackingPiece != null
             && attackingPiece.isWhite() != this.isWhite()
             && (attackingPiece instanceof Rook
                 || attackingPiece instanceof Queen
@@ -192,15 +148,15 @@ public abstract class Piece {
         attackingPiece = null;
         ti = kingSpot.getI()+1;
         tj = kingSpot.getJ()-1;
-        while(ti < 8 && tj >= 0) {
+        while (ti < 8 && tj >= 0) {
             attackingPiece = board.getSpot(ti, tj).getPiece();
-            if(attackingPiece != null) {
+            if (attackingPiece != null) {
                 break;
             }
             ti++;
             tj--;
         }
-        if(attackingPiece != null
+        if (attackingPiece != null
             && attackingPiece.isWhite() != this.isWhite()
             && (attackingPiece instanceof Bishop
                 || attackingPiece instanceof Queen
@@ -216,15 +172,15 @@ public abstract class Piece {
         attackingPiece = null;
         ti = kingSpot.getI()+1;
         tj = kingSpot.getJ()+1;
-        while(ti < 8 && tj < 8) {
+        while (ti < 8 && tj < 8) {
             attackingPiece = board.getSpot(ti, tj).getPiece();
-            if(attackingPiece != null) {
+            if (attackingPiece != null) {
                 break;
             }
             ti++;
             tj++;
         }
-        if(attackingPiece != null
+        if (attackingPiece != null
                 && attackingPiece.isWhite() != this.isWhite()
                 && (attackingPiece instanceof Bishop
                 || attackingPiece instanceof Queen
@@ -240,15 +196,15 @@ public abstract class Piece {
         attackingPiece = null;
         ti = kingSpot.getI()-1;
         tj = kingSpot.getJ()-1;
-        while(ti >= 0 && tj >= 0) {
+        while (ti >= 0 && tj >= 0) {
             attackingPiece = board.getSpot(ti, tj).getPiece();
-            if(attackingPiece != null) {
+            if (attackingPiece != null) {
                 break;
             }
             ti--;
             tj--;
         }
-        if(attackingPiece != null
+        if (attackingPiece != null
                 && attackingPiece.isWhite() != this.isWhite()
                 && (attackingPiece instanceof Bishop
                 || attackingPiece instanceof Queen
@@ -264,15 +220,15 @@ public abstract class Piece {
         attackingPiece = null;
         ti = kingSpot.getI()-1;
         tj = kingSpot.getJ()+1;
-        while(ti >= 0 && tj < 8) {
+        while (ti >= 0 && tj < 8) {
             attackingPiece = board.getSpot(ti, tj).getPiece();
-            if(attackingPiece != null) {
+            if (attackingPiece != null) {
                 break;
             }
             ti--;
             tj++;
         }
-        if(attackingPiece != null
+        if (attackingPiece != null
                 && attackingPiece.isWhite() != this.isWhite()
                 && (attackingPiece instanceof Bishop
                 || attackingPiece instanceof Queen
@@ -289,4 +245,53 @@ public abstract class Piece {
         end.setPiece(pieceCaptured);
         return false;
     }
+
+    private Spot getKingSpot(Board board, Spot start, Spot end) {
+        if (start.getPiece() instanceof King)
+            return end;
+
+        for (int row = 0; row < board.getSize(); row++) {
+            for (int column = 0; column < board.getSize(); column++) {
+                Piece piece = board.getSpot(row, column).getPiece();
+                if (piece instanceof King && piece.isWhite() == this.isWhite())
+                    return board.getSpot(row, column);
+            }
+        }
+
+        return board.getSpot(0,0); // this line will never be reached in a valid game
+    }
+
+    private boolean isKingSpotAttackedByPawn(Board board, Spot kingSpot) {
+        int ti = this.isWhite() ? kingSpot.getI()+1 : kingSpot.getI()-1; // the row on which an attacking pawn could be on
+        int tj = kingSpot.getJ(); // the column on which an attacking pawn could be on
+
+
+        Piece attackingPiece = board.getSpot(ti, tj-1) == null ? null : board.getSpot(ti, tj-1).getPiece();
+        if (attackingPiece instanceof Pawn && attackingPiece.isWhite() != this.isWhite())
+            return true;
+
+        attackingPiece = board.getSpot(ti, tj+1) == null ? null : board.getSpot(ti, tj+1).getPiece();
+        return (attackingPiece instanceof Pawn && attackingPiece.isWhite() != this.isWhite());
+    }
+
+    private boolean isKingSpotAttackedByKnight(Board board, Spot kingSpot) {
+        for (int ti = kingSpot.getI()-2; ti <= kingSpot.getI()+2; ti++) {
+            for (int tj = kingSpot.getJ()-2; tj <= kingSpot.getJ()+2; tj++) {
+                if (board.getSpot(ti, tj) != null) {
+                    Piece attackingPiece = board.getSpot(ti, tj).getPiece();
+                    int di = Math.abs(ti-kingSpot.getI());
+                    int dj = Math.abs(tj-kingSpot.getJ());
+
+                    if (di * dj == 2
+                            && attackingPiece instanceof Knight
+                            && attackingPiece.isWhite() != this.isWhite())
+                        return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
 }
