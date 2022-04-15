70de19660f186c26eae1bdda1ec8500303a7f944
vedanth <vedanth2805@gmail.com>
2022-04-15 09:42:04 +0530

    removed unnecessary Scanner import

--- chess-engine/src/main/java/com/jacob/engine/game/Game.java
+++ chess-engine/src/main/java/com/jacob/engine/game/Game.java
@@ -13,7 +13,6 @@ import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Game {
     private final Player[] players;
