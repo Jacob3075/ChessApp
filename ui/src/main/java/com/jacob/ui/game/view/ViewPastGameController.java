6016881fadbfe8ba30ed9f41f273c99e61508422
Lavanya-Y <lavanyamiliy@gmail.com>
2022-04-24 14:39:24 +0530

    GoBack button working now.

--- ui/src/main/java/com/jacob/ui/game/view/ViewPastGameController.java
+++ ui/src/main/java/com/jacob/ui/game/view/ViewPastGameController.java
@@ -4,22 +4,31 @@ import com.jacob.database.game_data.PastGame;
 import com.jacob.engine.game.Game;
 import com.jacob.ui.game.BoardController;
 import com.jacob.ui.game.MoveHistoryController;
+import com.jacob.ui.utils.JavaFxUtils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class ViewPastGameController {
+    @FXML private Button goBackButton;
     @FXML private HBox root;
     @FXML private MoveHistoryController moveHistoryController;
     @FXML private BoardController boardController;
+    private final ApplicationContext context;
     private PastGame pastGame;
     private final Game game;
 
-    public ViewPastGameController() {
+    public ViewPastGameController(ApplicationContext context) {
+        this.context = context;
         game = Game.createNewGame(true);
     }
 
@@ -27,6 +36,12 @@ public class ViewPastGameController {
         assert pastGame != null;
 
         boardController.initializeBoard(game.getBoard(), tile -> {});
+        goBackButton.setOnAction(this::showHomeScreen);
+    }
+
+    private void showHomeScreen(@NotNull ActionEvent event) {
+        Stage stage = (Stage) goBackButton.getScene().getWindow();
+        JavaFxUtils.changeScene(stage, JavaFxUtils.Views.SAVED_GAMES, context);
     }
 
     public void setDate(PastGame data) {
