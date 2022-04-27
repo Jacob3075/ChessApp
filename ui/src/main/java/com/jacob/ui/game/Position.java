681e9c68f8d0a788af43c5c24a30e36df7532f78
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-04-27 11:51:29 +0530

    Fixed wrong tile color.

--- ui/src/main/java/com/jacob/ui/game/Position.java
+++ ui/src/main/java/com/jacob/ui/game/Position.java
@@ -9,6 +9,6 @@ public record Position(int row, int column, int index, boolean isWhiteCell) {
     }
 
     private Position(int row, int column, int index) {
-        this(row, column, row * 8 + column, (index + (row % 2 == 0 ? 0 : 1)) % 2 == 0);
+        this(row, column, row * 8 + column, (index + (row % 2 == 0 ? 1 : 0)) % 2 == 0);
     }
 }
